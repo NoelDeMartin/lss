c67ecb222001062752cc4f16a263775e30bbd131
Noel De Martin <noel@noeldemartin.com>
2024-08-16 08:56:40 +0200

    Start

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>LSS</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    <body class="font-sans antialiased">
+        <div class="flex items-center justify-center w-screen h-screen">
+            <h1 class="font-semibold text-4xl">LSS</h1>
+        </div>
+    </body>
+</html>
