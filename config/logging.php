2c2dd59db6ae07fe87e7f9758b8a03c83f9b0cff
Noel De Martin <noel@noeldemartin.com>
2025-01-10 09:04:37 +0100

    Log emails in production

--- config/logging.php
+++ config/logging.php
@@ -73,6 +73,13 @@ return [
             'replace_placeholders' => true,
         ],
 
+        'mails' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/mails.log'),
+            'level' => 'debug',
+            'replace_placeholders' => true,
+        ],
+
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
