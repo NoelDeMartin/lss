1ea926a2356e230b531473be84c4c5a7fe589ce0
Noel De Martin <noel@noeldemartin.com>
2025-02-04 16:36:26 +0100

    Fix linting errors

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable implements MustVerifyEmail
     protected static $urlGenerator = null;
 
     public $cloud_folder = 'Solid';
+    public $cloudSyncFailed = false;
 
     protected $cloud = null;
 
