0ea7c3ac626b67a60e342d23278880223cd7d371
Noel De Martin <noel@noeldemartin.com>
2025-02-09 10:18:53 +0100

    Improve UI for expired verification links

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\HandleCors;
+use Illuminate\Routing\Exceptions\InvalidSignatureException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +16,11 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->replace(HandleCors::class, AppHandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (InvalidSignatureException $e) {
+            if (url()->signatureHasNotExpired(request())) {
+                return;
+            }
+
+            return response()->view('auth.verify-email', ['expired' => true]);
+        });
     })->create();
--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,6 +1,10 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+        @isset($expired)
+            {{ __('This verification link has expired, you\'ll need to get a new one!') }}
+        @else
+            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+        @endisset
     </div>
 
     @if (session('status') == 'verification-link-sent')
